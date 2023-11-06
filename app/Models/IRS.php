5ff0a465acb77e5eb8674f8f9b5b78580cbc9e2f
ZZIDZZ <zidanutomo@gmail.com>
2023-11-07 00:28:13 +0700

    rename all akademik model to match orm

--- app/Models/IRS.php
+++ app/Models/IRS.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class IRS extends Model
+class Irs extends Model
 {
 
     // Schema::create('irs', function (Blueprint $table) {
--- app/Models/KHS.php
+++ app/Models/KHS.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class KHS extends Model
+class Khs extends Model
 {
 
     // Schema::create('khs', function (Blueprint $table) {
