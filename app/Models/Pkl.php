26dc9f7fbacd9448358170ffe2adf9686c5868fb
zidan utomo <zidanutomo@gmail.com>
2023-11-15 12:02:23 +0700

    added pkl

--- app/Models/Pkl.php
+++ app/Models/Pkl.php
@@ -197,8 +197,9 @@ class Pkl extends Model
             'mahasiswa_id' => $mahasiswa_id
         ];
         $total_sks = DB::select($query, $params)[0]->total_sks;
-        if ($total_sks < 100) {
-            throw new \Exception("Total SKS kurang dari 100, tidak bisa membuat PKL");
+        if ($total_sks < 80) {
+            dd($total_sks);
+            throw new \Exception("Total SKS kurang dari 80, tidak bisa membuat PKL");
         }
 
 
