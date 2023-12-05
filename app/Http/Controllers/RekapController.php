7ad6f604436b22a67e9aa22123b3e8e6fadbda47
ZZIDZZ <zidanutomo@gmail.com>
2023-12-05 19:48:56 +0700

    asdf

--- app/Http/Controllers/RekapController.php
+++ app/Http/Controllers/RekapController.php
@@ -415,7 +415,10 @@ class RekapController extends Controller
         // change reminder_day_config to integer
         // Fetch data from the SQL query
         $sql = "SELECT * FROM (
-            SELECT semester.semester, 
+            SELECT irs.semester as semester,
+            CASE 
+            WHEN irs.id IS NOT NULL THEN true 
+            ELSE false END as is_irs, 
             CASE 
             WHEN k.id IS NOT NULL THEN true 
             ELSE false END as is_khs, 
@@ -425,24 +428,17 @@ class RekapController extends Controller
             CASE 
             WHEN s.id IS NOT NULL THEN true 
             ELSE false END as is_skripsi,
-            irs_mahasiswa.id, 
-            irs_mahasiswa.sks_semester as sks_semester, 
+            irs.id, 
+            irs.sks_semester as sks_semester, 
             k.ip_semester as ip_semester,
             p.nilai as nilai_pkl,
-            s.nilai as nilai_skripsi,
-            sa.tahun_ajaran as tahun_ajaran, 
-            sa.semester as semester_akademik
-            FROM generate_series(1, 14) AS semester
-            LEFT JOIN 
-            (
-                SELECT irs.*, ROW_NUMBER() OVER (ORDER BY id) as irs_number FROM irs WHERE mahasiswa_id=:mahasiswa_id ORDER BY id
-            ) irs_mahasiswa 
-            ON irs_mahasiswa.irs_number = semester.semester
-            LEFT JOIN semester_akademik sa ON sa.id = irs_mahasiswa.semester_akademik_id
-            LEFT JOIN khs k ON k.irs_id = irs_mahasiswa.id
-            LEFT JOIN pkl p ON p.irs_id = irs_mahasiswa.id
-            LEFT JOIN skripsi s ON s.irs_id = irs_mahasiswa.id
-            ORDER BY semester.semester
+            s.nilai as nilai_skripsi
+            FROM irs
+            LEFT JOIN khs k ON k.mahasiswa_id = irs.mahasiswa_id AND k.semester = irs.semester
+            LEFT JOIN pkl p ON p.mahasiswa_id = irs.mahasiswa_id AND p.semester = irs.semester
+            LEFT JOIN skripsi s ON s.mahasiswa_id = irs.mahasiswa_id AND s.semester = irs.semester
+            WHERE irs.mahasiswa_id = :mahasiswa_id
+            ORDER BY irs.semester
             ) as dummy
             ";
         // dd("
